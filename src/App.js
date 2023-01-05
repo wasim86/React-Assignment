db0e36f658e7d54bf9d12ba040ab1ccbaa65d0b1
wasim86 <mohdwasim1996@gmail.com>
2023-01-05 22:48:58 +0530

    done

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import logo from './logo.svg';
+import './App.css';
+import Textarea from './components/Textrea';
+import Button from './components/Button'
+import React,{useState } from 'react';
+
+
+
+function App()  {
+  
+  return(
+    <div>
+      <h1>This my Today Assignment</h1>
+    <Button/>
+    </div>
+    
+  )
+   
+}
+
+
+    
+
+export default App;
